c65353aa59c03749c4f8207397a7cf62e8a29bce
deeamtee <iceforcy@gmail.com>
2018-10-18 17:25:39 +0300

    User registration: user login user reset password (email is not work, need to enable django mail)

--- account/views.py
+++ account/views.py
@@ -6,29 +6,35 @@ from django.views.generic.base import View
 
 from .forms import EmailPostForm
 
+
 class LoginView(TemplateView):
-    template_name = 'account/login.html'
+    template_name = 'registration/login.html'
+
     def login(request):
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user is not None and user.is_active:
-                # Правильный пароль и пользователь "активен"
-                auth.login(request, user)
-                # Перенаправление на "правильную" страницу
-                return HttpResponseRedirect("/account/loggedin/")
-            else:
-                # Отображение страницы с ошибкой
-                return HttpResponseRedirect("/account/invalid/")
+        username = request.POST['username']
+        password = request.POST['password']
+        print('Получил username и password!')
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None and user.is_active:
+            print('Я тут был!')
+            # Правильный пароль и пользователь "активен"
+            auth.login(request, user)
+            # Перенаправление на "правильную" страницу
+            return HttpResponseRedirect("/registration/loggedin/")
+        else:
+            print('Ошибка! но тут был!')
+            # Отображение страницы с ошибкой
+            return HttpResponseRedirect("/registration/invalid/")
 
 
 def logout(request):
     auth.logout(request)
     # Перенаправление на страницу.
-    return HttpResponseRedirect("/account/loggedout/")
+    return HttpResponseRedirect("/registration/loggedout/")
 
 
-def testform(request):# Retrieve post by id
+def testform(request):  # Retrieve post by id
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
@@ -38,4 +44,4 @@ def testform(request):# Retrieve post by id
             # ... send email
     else:
         form = EmailPostForm()
-    return render(request, 'account/testform.html', {'form': form})
+    return render(request, 'registration/testform.html', {'form': form})
