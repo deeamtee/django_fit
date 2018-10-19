ee74d5445fb286d562186d4330b7de8a84f01f95
deeamtee <iceforcy@gmail.com>
2018-10-19 13:26:13 +0300

    Delete account application

--- account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-from .views import *
-
-urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('test/', testform)
-]
