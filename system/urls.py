2ea1aadbfe6ca2c29e4a6e66b81e89a41dffc37d
deeamtee <iceforcy@gmail.com>
2018-11-03 19:07:39 +0300

    add css for login page

--- system/urls.py
+++ system/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from system.views import index, logout, login
+from system.views import index
 
 urlpatterns = [
     path('', include('allauth.urls')),
     # path('', include('django.contrib.auth.urls')),
-    path('index/', index),
+    path('index/', index)
 
 ]
--- system/views.py
+++ system/views.py
@@ -1,31 +1,7 @@
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render
 
 
 def index(request):
-    template = 'user/index.html'
-    context = {}
-    return render(request, template, context)
-
-
-# def register(request):
-#     form = UserCreationForm()
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
-#             login(request, user)
-#             return redirect('index')
-#
-#     template = 'account/register.html'
-#     context = {'form': form}
-#     return render(request, template, context)
-
-
-def logout(request):
     template = 'user/index.html'
     context = {}
     return render(request, template, context)
